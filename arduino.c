2eca4e87d210ab24b799b0c743649b8cbec7e1f0
Pieski <tinesharp@outlook.com>
2022-04-05 19:49:19 +0800

    清明版
    删除磁导航，完善通信，加入超时保分策略

--- arduino.c
+++ arduino.c
@@ -5,6 +5,7 @@
 #define PULSE 0xBB
 #define RUN 0xCC
 
+//串口通信字节
 #define WAKE_BYTE '+'
 #define STAGE_A_BYTE 0xA0
 #define STAGE_B_BYTE 0xB0
@@ -15,6 +16,9 @@
 #define C_REV 0x0B
 #define C_LFT 0x0C
 #define C_RGT 0x0D
+
+//强制回程时间 ms
+#define TIME_MAX 480000			//8min
 
 //循迹导航模式
 #define COURSE_FWD 0
@@ -43,16 +47,19 @@
 #define CENTER 33
 #define RIGHT 32
 #define RIGHTSIDE 30
-#define REV_LEFTSIDE 40
+#define REV_LEFTSIDE 29
 #define REV_LEFT 46
 #define REV_RIGHT 47
-#define REV_RIGHTSIDE 41
+#define REV_RIGHTSIDE 39
 
 //超声PIN
-#define HCL_TRIG 48
-#define HCL_ECHO 49
+#define HCL_TRIG 43
+#define HCL_ECHO 41
 #define HCR_TRIG 45
 #define HCR_ECHO 44
+
+//电磁铁继电器PIN
+#define RELAY 40
 
 //强制刹车时间
 #define BRAKING 1000
@@ -79,8 +86,8 @@
 //强制转向时间
 #define TURNNING 600
 
-//强制回正时间
-#define TURNING_RETURN 170
+//强制回正时间 170
+#define TURNING_RETURN 1
 
 //转向刹车时间
 #define TURNING_BRAKE 1000
@@ -101,7 +108,7 @@
 #define END_REVERSE 1000
 
 //刹车距离(cm)
-#define STOP_DIST_A 10.00
+#define STOP_DIST_A 20.00
 #define STOP_DIST_B 5.00
 
 //吸合距离(cm)
@@ -111,41 +118,13 @@
 #define TIMEOUT 10000
 
 //超声允许误差
-#define SONAR_DIFF_MAX 3.0
+#define SONAR_DIFF_MAX 0.3
 
 //超声修正转速
-#define SOR_CORRECT_SPD 200
+#define SOR_CORRECT_SPD 150
 
 //超声单步时间
 #define SOR_STEP_TIME 100
-
-//磁航向修正转速
-#define MAG_CORRECT_SPD 200
-
-//磁航向修正单步时间(ms)
-#define MAG_STEP_TIME 100
-
-//读磁航向超时
-#define MAG_ERROR 99999
-
-//读磁航向置信阈值
-#define MAG_DIFF_MAX 360.0
-
-//磁校准置信阈值
-#define MAG_CALI_DIFF_MIX 5.0
-
-//磁航向修正允许误差
-#define MAG_DIFF_MIN 3.0
-
-//读磁航向滤波次数
-#define MAG_DETECT_TIME 5
-
-//磁校准滤波次数
-#define MAG_CALI_TIME 20
-
-//磁校准读取次数上限
-#define MAG_ERROR_TIME 10
-
 
 /*状态机
 	FWD：循迹直行
@@ -157,6 +136,8 @@
 	WAT：等待
 	PUL：进入PULSE模式
 	REF：基准航向设置（刹车）
+	SOR：超声校准
+	KEYNODE：关键节点标志
 */
 #define FWD 0
 #define STP 1
@@ -168,8 +149,10 @@
 #define PUL 7
 #define REF 8
 #define MAG 9
+#define SOR 10
+#define KEYNODE 0xFF
 
-#define STORAGE_ON_LEFT {FWD,1},{LFT,1},{STP,1},{REV,2},{RGT,1},{MAG,1}
+#define STORAGE_ON_LEFT {KEYNODE,1},{LFT,1},{STP,1},{REV,2},{RGT,1}
 
 void correct(int);
 void forward();
@@ -181,11 +164,6 @@ void writespeed(int,int);
 int course(int);
 void course_block(int);				//阻塞
 void course_time(int,int);			//阻塞，非精确时间，非常不精确
-									
-double m_angle();					//获取当前磁角度 还没写
-double m_update(int);				//计算转向后的航向
-double m_diff(double,double);		//计算两个磁角间的劣角
-void m_recali();					//传感器重置
 
 double s_dist(int);					//读超声距离
 void s_cali();						//超声对齐
@@ -193,10 +171,14 @@ void s_cali();						//超声对齐
 //总状态机
 int stat = RUN;
 
+//比赛开始时间
+long zerotime = 0;
+
 //任务流程。第一索引标记一级步骤，第二索引标记步骤类型及重复次数，下面称一级步骤中重复执行的部分为二级步骤
-short procedure[][2] = {{REF,1},{FWD,1},{LFT,1},{FWD,3},STORAGE_ON_LEFT,STORAGE_ON_LEFT,STORAGE_ON_LEFT,{OVR,1}};
+short procedure[][2] = {{FWD,1},{LFT,1},{FWD,2},STORAGE_ON_LEFT,{FWD,2},STORAGE_ON_LEFT,{FWD,2},STORAGE_ON_LEFT,{FWD,1},{SOR,1},{REV,3},{FWD,1},{RGT,1},{FWD,2},{OVR,1}};
 short step_rp = 0;
 short step = 0;
+int procedure_len = 0;
 
 //红外状态
 short ir_map[5] = {1,1,1,1,1};
@@ -217,14 +199,6 @@ int speed = SPD_STAND;
 
 //目标距离
 double dist = 0.00;
-
-//基准航向
-double ref_heading = 0.00;
-
-double cali = 0.00;
-
-//当前正确航向
-double yaw_set = 0.00;
 
 void setup(){
 	pinMode(LEFT_POS_A, OUTPUT);
@@ -250,21 +224,17 @@ void setup(){
 	pinMode(HCL_TRIG, OUTPUT);
 	pinMode(HCR_ECHO, INPUT);
 	pinMode(HCR_TRIG, OUTPUT);
-
-	delay(3000);
-
-	Serial.begin(115200);
-  	Serial.print(0xAA);
-
-  	Serial2.begin(9600);
-  	Serial2.println("AT+PRATE=100");
-
-  	while(Serial.read() != WAKE_BYTE);
-  	Serial.println("Start initializing");
-
-	setlogic(1,0,1,0);
+	pinMode(RELAY, OUTPUT);
 	digitalWrite(HCL_TRIG, 0);
 	digitalWrite(HCR_TRIG, 0);
+	setlogic(0,0,0,0);
+
+	Serial.begin(115200);
+	while(Serial.read() != WAKE_BYTE);
+
+	zerotime = millis();
+	setlogic(1,0,1,0);
+
 
 	for(int i=0;i<3;++i){
 	    ir_map[0] = (short)digitalRead(LEFTSIDE);
@@ -275,11 +245,9 @@ void setup(){
   	}
 }
 
-double last_read,read;
 void loop(){
 	if(DEBUG == 1){
-		m_recali();
-		delay(20000);
+		Serial.println(s_dist(0));
 		return;
 	}
 
@@ -303,13 +271,21 @@ void loop(){
 		else
 			setlogic(1,0,1,0);
 
+		//如果在左转去货架的关键节点超时，则取消左转
+		if(procedure[step][0] == KEYNODE){
+			if(millis() - zerotime >= TIME_MAX){
+				procedure_fold(step,5,procedure_len);
+				procedure_len -= 5;
+			}
+		}
+
 		//检测当前一级步骤是否已经重复执行完毕
 		if(step_rp >= procedure[step][1]){
 
 			//在开始下一步前进行特殊步骤的过渡
 			//倒行 -> 转向：继续倒车离开路口，然后重新直行循迹到这个路口，再转向
 			if(procedure[step][0] == REV && (procedure[step+1][0] == LFT || procedure[step+1][0] == RGT)){
-				course_time(COURSE_REV, 1000);
+				course_time(COURSE_REV, 600);
 				brake(BRAKE_STABLE);
 				setlogic(1,0,1,0);
 				rev_flag = 0;
@@ -361,23 +337,22 @@ void loop(){
 			delayMicroseconds(10);
 			digitalWrite(HCR_TRIG,LOW);
 			dist = (double)pulseIn(HCR_ECHO, HIGH, TIMEOUT) / 58.0;
-			Serial.println(dist);
 			if(stop_flag == 0){
 				if(dist <= STOP_DIST_A && dist > 0.10){
-					Serial.println("stopped at position A");
 					brake(BRAKING);
 					s_cali();
-					Serial.print(STAGE_A_BYTE);
+					Serial.write(STAGE_A_BYTE);
+					//delay(3000);
 					while(Serial.read() != WAKE_BYTE);
 					stop_flag = 1;
 					break;
 				}
 			}else if(stop_flag == 1){
 				if(dist <= STOP_DIST_B && dist > 0.10){
-					Serial.println("stopped at position B");
 					brake(BRAKING);
 					s_cali();
-					Serial.print(STAGE_B_BYTE);
+					Serial.write(STAGE_B_BYTE);
+					//delay(3000);
 					while(Serial.read() != WAKE_BYTE);
 					stop_flag = 0;
 					step_rp ++;
@@ -405,35 +380,8 @@ void loop(){
 			step_rp ++;
 			break;
 
-			case REF:
-			brake(BRAKE_STABLE);
-			Serial2.println("AT+INIT");
-			delay(500);
-			cali = 0.0;
-		    last_read = 10000.0;
-		    read = 0.0;
-			for(int i=0;i<MAG_CALI_TIME;++i){
-				read = m_angle();
-				if(last_read != 10000.0 && abs(read - last_read) > MAG_CALI_DIFF_MIX){
-					i = 0;
-					cali = 0;
-					read = 0.0;
-					last_read = 10000.0;
-					continue;
-				}
-				cali += read;
-				delay(10);
-			}
-			yaw_set = cali/MAG_CALI_TIME;
-			ref_heading = yaw_set;
-			Serial.print("yaw_set = ");
-			Serial.print(yaw_set);
-			Serial.print("\n");
-			step_rp ++;
-			break;
-
-			case MAG:
-			m_cali();
+			case SOR:
+			s_cali();
 			step_rp++;
 			break;
 		}
@@ -443,7 +391,6 @@ void loop(){
 	}
 
 	else if(stat == PULSE){
-		Serial.println("pulsed");
 		delay(1000);
 		return;
 	}
@@ -457,7 +404,6 @@ void loop(){
 	}
 }
 
-//若缓冲区有数据，则在loop()间隙触发
 void serialEvent(){
 	Serial.print("detected = ");
 	int recv = Serial.read();
@@ -646,80 +592,6 @@ void course_time(int mode, int time){
 	}
 }
 
-//读取当前磁角度
-double m_angle(){
-	int readcount = 0;
-	while(Serial2.read()!='w' && readcount <= MAG_ERROR)
-		readcount++;
-
-	if(readcount >= MAG_ERROR)
-		return 720.0;
-
-	char buf[10];
-	Serial2.readBytes(buf,10);
-
-	double r = 0.0;
-	int minflag = 1;
-	byte potflag = 0;
-	int ncount_a = 0, ncount_b = 0;
-	int num[6] = {0};
-
-	for(int i=0;i<10;++i){
-		if(buf[i] == '.')
-			potflag = 1;
-		if(buf[i] >= 48 && buf[i] <= 57){
-			num[ncount_a+ncount_b] = buf[i] - 48;
-			if(potflag)
-				ncount_b++;
-			else
-				ncount_a++;
-		}
-		if(buf[i] == '-')
-			minflag = -1;
-	}
-
-	/*for(int i=0;i<6;++i)
-		Serial.print(num[i]);
-
-	Serial.print("   ncount_a = ");
-	Serial.print(ncount_a);
-	Serial.print("   minflag = ");
-	Serial.print(minflag);
-	Serial.print("\n");*/
-
-	int a = ncount_a + ncount_b - 1;
-	for(int i=0;i<ncount_b;++i)
-		r += pow(10,-1*ncount_b+i) * num[a-i];
-	for(int i=0;i<ncount_a;++i)
-		r += pow(10,i) * num[a-ncount_b-i];
-	
-	Serial2.println("AT+INIT");
-	return r * (double)minflag;
-}
-
-//计算转向后的设定航向，mode=1：左转
-double m_update(int mode){
-	if(mode == 1)
-		yaw_set += 90.0;
-	else if(mode == 0)
-		yaw_set -= 90.0;
-	if(yaw_set > 180.0)
-		yaw_set -= 360.0;
-	else if(yaw_set < -180.0)
-		yaw_set += 360.0;
-
-	Serial.print("yaw set to");
-	Serial.print(yaw_set);
-	Serial.print("\n");
-	return yaw_set;
-}
-
-double m_diff(double a, double b){
-	if(abs(a-b) > 180)
-		return abs(360 - abs(a-b));
-	return abs(a-b);
-}
-
 //超声测距，side = 0：左侧
 double s_dist(int side){
 	if(side == 0){
@@ -735,91 +607,30 @@ double s_dist(int side){
 	}
 }
 
+double distl,distr;
 void s_cali(){
-	double distl,distr;
-	do{
+	distl = s_dist(0);
+	distr = s_dist(1);
+	while((double)abs(distl - distr) >= SONAR_DIFF_MAX){
 		distl = s_dist(0);
 		distr = s_dist(1);
-		Serial.print("distl = ");
-		Serial.print(distl);
-		Serial.print("   distr = ");
-		Serial.print(distr);
-		Serial.print("\n");
 		if(distl > distr)
 			setlogic(1,0,1,0);
 		else
 			setlogic(0,1,1,0);
 		writespeed(SOR_CORRECT_SPD,0);
 		delay(SOR_STEP_TIME);
-	}while((double)abs(distl - distr) >= SONAR_DIFF_MAX);
+	}
+	brake(BRAKE_STABLE);
 }
 
-void m_cali(){
-	Serial.print("ref_heading = ");
-	Serial.print(ref_heading);
-	Serial.print("diff = ");
-	Serial.print(m_diff(yaw_set, ref_heading));
-	Serial.print("\n");
-
-	double current_yaw = 0.0;
-	for(int i=0;i<MAG_DETECT_TIME;++i){
-		current_yaw += m_angle();
-		delay(20);
+void procedure_fold(int start, int num, int len){
+	for(int i=start;i<len-num;++i){
+		procedure[i][0] = procedure[i+num][0];
+		procedure[i][1] = procedure[i+num][1];
 	}
-	current_yaw /= MAG_DETECT_TIME;
-
-	Serial.print("current_yaw = ");
-	Serial.print(current_yaw);
-	Serial.print("      yaw_set = ");
-	Serial.print(yaw_set);
-	Serial.print("      diff = ");
-	Serial.print(m_diff(current_yaw, yaw_set));
-	Serial.print("\n");
-
-	int trytime = 0;
-	for(;trytime<3;++trytime){
-		if(m_diff(current_yaw, yaw_set) <= MAG_DIFF_MAX)
-			break;
-		current_yaw = m_angle();
-		delay(20);
+	for(int i=len-num;i<len;++i){
+		procedure[i][0] = 0;
+		procedure[i][1] = 0;
 	}
-
-	if(trytime <= 3){
-		if(m_diff(current_yaw, yaw_set) <= MAG_DIFF_MAX){
-			while(m_diff(current_yaw, yaw_set) > MAG_DIFF_MIN){
-				Serial.print("current_yaw = ");
-				Serial.print(current_yaw);
-				Serial.print("\n");
-				if(current_yaw < yaw_set){
-					writespeed(0,MAG_CORRECT_SPD);
-					delay(MAG_STEP_TIME);
-				}else{
-					writespeed(MAG_CORRECT_SPD,0);
-					delay(MAG_STEP_TIME);
-				}
-				brake(TURNING_BRAKE);
-
-				current_yaw = 0.0;
-				for(int i=0;i<MAG_DETECT_TIME;++i){
-					current_yaw += m_angle();
-					delay(20);
-				}
-				current_yaw /= MAG_DETECT_TIME;
-			}
-		}
-	}
-}
-
-void m_recali(){
-	Serial2.println("AT+INIT");
-	delay(500);
-	Serial2.println("AT+PRATE=0");
-	delay(500);
-	Serial2.println("AT+CALI=1");
-	setlogic(1,0,0,1);
-	writespeed(SPD_STAND,SPD_STAND);
-	delay(20000);
-	writespeed(0,0);
-	setlogic(0,0,0,0);
-	Serial2.println("AT+CALI=0");
 }
